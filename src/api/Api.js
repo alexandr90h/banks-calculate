37c91886b9de49971b1caeb6d6813fb4e9419a77
Alexandr <63474923+alexandr90h@users.noreply.github.com>
2021-02-06 07:20:51 +0200

    06.02.2021-1

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-axios.defaults.baseURL =
-  "https://my-json-server.typicode.com/alexandr90h/calc-mortgage";
+axios.defaults.baseURL = "https://api.jsonbin.io/b/601db51bc033606410a7f18f";
+// axios.defaults.headers = {
+//   "secret-key": "$2b$10$smdXgZXOVoJ.0kS/lwTdQuKTGQz./wsG.kQEWvqWihEBga9cMdj1W",
+//   name: "banks",
+// };
 export async function fetchBanks() {
   return await axios.get("/banks/");
 }
 export async function AddBanks(item) {
-  return await axios.post("/banks/", item);
+  return await axios.put("/banks/", item);
 }
